7c942beb0e9a549f2d25d779ae009e4440e3819e
kojima1021 <kojima1021_sr@yahoo.co.jp>
2014-04-06 22:58:21 +0900

    ソースをきれいにした少しだけ

--- src/main/java/net/bukkit_plugin/kojima1021/KillLog/Event/OnDeathEvent.java
+++ src/main/java/net/bukkit_plugin/kojima1021/KillLog/Event/OnDeathEvent.java
@@ -19,17 +19,17 @@ public class OnDeathEvent implements Listener {
 		 if (e.getEntity().getKiller() != null){
 		    e.getEntity().getPlayer().getServer().getLogger().info("");
 		    String prefix = ChatColor.RED + "[" + ChatColor.WHITE + "PVPLog" + ChatColor.RED + "]" + ChatColor.RESET;
-			Calendar now = Calendar.getInstance();
-			int h = now.get(now.HOUR_OF_DAY);//時を取得
+		    Calendar now = Calendar.getInstance();
+		    int h = now.get(now.HOUR_OF_DAY);//時を取得
 		    int m = now.get(now.MINUTE);     //分を取得
 		    int s = now.get(now.SECOND);      //秒を取得
-		     int a = (int) e.getEntity().getKiller().getHealth();
-		     int b = (int) 2;
-		     e.setDeathMessage(prefix + "  "+e.getEntity().getPlayer().getName() + "は" + e.getEntity().getKiller().getName() + "に殺された。   時刻 "+ h+"時"+m+"分"+s+"秒" );
-		 	 e.getEntity().getKiller().sendMessage(prefix +" あなたは、" + e.getEntity().getPlayer().getName() + " を倒した。   (自分の残りのハートの数は " +a / b +"個です。 )" );
-		 	 return;
-			}else {
+		    int a = (int) e.getEntity().getKiller().getHealth();
+		    int b = (int) 2;
+		    e.setDeathMessage(prefix + "  "+e.getEntity().getPlayer().getName() + "は" + e.getEntity().getKiller().getName() + "に殺された。   時刻 "+ h+"時"+m+"分"+s+"秒" );
+		    e.getEntity().getKiller().sendMessage(prefix +" あなたは、" + e.getEntity().getPlayer().getName() + " を倒した。   (自分の残りのハートの数は " +a / b +"個です。 )" );
+		    return;
+		 }else {
 			e.setDeathMessage(e.getDeathMessage());
 			}
-}
+	}
 }
